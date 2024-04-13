3fc5e9aaa4ad5fa33ed74aa2882ca80d3049ac3e
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2024-04-12 20:48:59 -0400

    enabled zipcode selection

--- publixalert.py
+++ publixalert.py
@@ -3,18 +3,36 @@ from fuzzywuzzy import fuzz
 import requests
 from bs4 import BeautifulSoup
 
+# Database (TEMP: MOCK)
 bogo_items = []
 
-response = requests.get('https://accessibleweeklyad.publix.com/PublixAccessibility/BrowseByListing/ByCategory/?ListingSort=8&StoreID=2729441&CategoryID=5232540')
-soup = BeautifulSoup(response.content, 'html.parser')
-bogo_deals = soup.find_all('div', class_='unitB')
+# Get Store ID
+def get_store_id(zipcode):
+    response = requests.get('https://accessibleweeklyad.publix.com/PublixAccessibility/StoreLocation/Index/?StoreID=2729441&CityStateZip=' + zipcode)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-for deal in bogo_deals:
-    product_name = deal.find('h2', class_='ellipsis_text').text
-    bogo_items.append(product_name)
+    store = soup.find('div', class_='storeLocation_listRepeater')
 
+    if store:
+        store_id = store.find('a', class_='action-tracking-nav')['href'].split('=')[-1]
+        print(store_id)
+        return store_id
+    else:
+        print("No store found")
+        return None
 
-# Functions
+# Get BOGO Deals
+def get_bogo_deals(store_id):
+    response = requests.get('https://accessibleweeklyad.publix.com/PublixAccessibility/BrowseByListing/ByCategory/?&CategoryID=5232540&StoreID=' + store_id)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    bogo_deals = soup.find_all('div', class_='unitB')
+
+    # Get product names
+    for deal in bogo_deals:
+        product_name = deal.find('h2', class_='ellipsis_text').text
+        bogo_items.append(product_name)
+
+# Search for items in the shopping list
 def find_items(shopping_list, bogo_items):
     matching_items = []
 
@@ -40,13 +58,15 @@ st.divider()
 # User Settings Page
 st.subheader('User Settings')
 
-phone_number = st.text_input('Enter your phone number:')
-local_store = st.text_input('Enter the address for your local Publix:')
-
-shopping_list_input = st.text_area('Enter your Shopping list:')
+phone_number = st.text_input('Enter Your Phone Number:')
+zipcode = st.text_input('Enter Your Zipcode:')
+shopping_list_input = st.text_area('Enter Your Shopping list:')
 
 
 if st.button('Save'):
+    store_id = get_store_id(zipcode)
+    get_bogo_deals(store_id)
+
     shopping_list = shopping_list_input.split("\n")
     shopping_list = [item.strip() for item in shopping_list if item.strip()]
     matching_list = find_items(shopping_list, bogo_items)
